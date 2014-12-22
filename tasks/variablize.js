584ff1041225fc042642157b00d1244564dd1989
Remi Barraquand <dev@remibarraquand.com>
2014-12-22 12:19:30 +0100

    extend variablize to support any text file

--- tasks/variablize.js
+++ tasks/variablize.js
@@ -3,27 +3,36 @@
 module.exports = function (grunt) {
     'use strict';
 
+    var fs = require('fs');
+    var _ = require('lodash');
     var path = require('path');
     var utils = require('./utils').init(grunt);
 
-    grunt.registerMultiTask('variablize', 'Turn SVG file into js variable.', function () {
-        var options = this.options();
+    grunt.registerMultiTask('variablize', 'Turn file into js variable.', function () {
+        var globals = this.options({
+            encoding: 'utf8'
+        });
 
-        var src = this.data.src  || grunt.log.error('`src` is not defined');
-        var dest = this.data.dest || grunt.log.error('`dest` is not defined.');
-        var variable = this.data.variable || grunt.log.error('`variable` is not defined.');
+        this.files.forEach(function(file) {
+            var src = file.src[0] || grunt.log.error('`src` is not defined');
+            var dest = file.dest || grunt.log.error('`dest` is not defined.');
+            var variable = file.variable || grunt.log.error('`variable` is not defined.');
+            var options = _.merge({}, globals, file.options || {});
 
-        if (!grunt.file.exists(src)) {
-            grunt.log.warn("File "+src+" does not appear to exist.");
-        }
+            if (!grunt.file.exists(src)) {
+                grunt.log.warn("File " + src + " does not appear to exist.");
+            }
 
-        if (path.extname(src) === '.svg') {
-            var xml_as_string = utils.xml2String(src, options.svgo).then(function(data) {
-                var output = "var " + variable + " = '" + data + "';";
+            if (path.extname(src) === '.svg') {
+                var xml_as_string = utils.xml2String(src, options.svgo).then(function (data) {
+                    var output = "var " + variable + " = '" + data + "';";
+                    grunt.file.write(dest, output);
+                });
+            } else {
+                var src_as_string = fs.readFileSync(src, options.encoding);
+                var output = "var " + variable + " = '" + utils.jsEscape(src_as_string) + "';";
                 grunt.file.write(dest, output);
-            });
-        } else {
-            grunt.log.warn("Source file specified by `src` must be an SVG file.")
-        }
+            }
+        });
     });
 };
